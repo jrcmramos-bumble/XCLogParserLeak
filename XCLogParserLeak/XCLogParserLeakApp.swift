54ae335c941c7436ad7ec7485ea987f568a44e09
Jose Ramos <jose.ramos@team.bumble.com>
2024-10-14 13:38:35 +0100

    Initial Commit

---
+++ XCLogParserLeak/XCLogParserLeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCLogParserLeakApp.swift
+//  XCLogParserLeak
+//
+//  Created by Jose Correia Miranda Ramos on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct XCLogParserLeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
